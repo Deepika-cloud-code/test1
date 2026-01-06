a2c58faf20692d606929e22f05b7b7fd1ec45a2a
Deepika-cloud-code <vaddadi.d@northeastern.edu>
2026-01-06 17:12:48 -0500

    thirsty block added: drink water

--- hungry.py
+++ hungry.py
@@ -1,5 +1,10 @@
 hungry=input("Are you hungry?")
 if hungry == "yes":
     print("Eat Fruits")
+    print("Eat Pizza")
+    print("Eat Burger")
 else:
-    print("do your homework")
+    thirsty=input("Are you thirsty?")
+    if thirsty == "yes":
+        print("Drink Water")
+        
