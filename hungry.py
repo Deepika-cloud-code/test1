716bfba105af67cfaadbcbee1ae2cac34daed549
Deepika-cloud-code <vaddadi.d@northeastern.edu>
2026-01-06 19:17:45 -0500

    added maybe elif

--- hungry.py
+++ hungry.py
@@ -9,6 +9,8 @@ elif hungry == "no":
     if thirsty == "yes":
         print("Drink Water")
         print("Drink Soda drink")
+elif hungry == "maybe":
+    print("Have snacks and drink water")
 else:
     print("sorry, you are neither hungry not thirsty")
         
